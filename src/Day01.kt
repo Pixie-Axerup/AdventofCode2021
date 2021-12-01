ee8a13299af2ea59ee54074bb24d3effd1ff3839
Pixie Axerup <pixie.axerup@ingka.ikea.com>
2021-12-01 14:19:42 +0100

    day1 done

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,34 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+
+    val depth = readInput("Day01")
+
+    fun part1(): Int {
+        return depth.map {
+            it.toInt()
+        }
+            .zipWithNext()
+            .count {
+                    (first, second) -> second > first
+            }
+
+
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(): Int {
+        return depth.asSequence()
+            .map {
+                it.toInt()
+            }
+            .windowed(3)
+            .map {
+                it.sum()
+            }
+            .zipWithNext()
+            .count {
+                    (first, second) -> second > first
+            }
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    println(part1())
+    println(part2())
 }
